8ec6df41870c41f0227b68cf910162ae89ed49d5
ShotaL <52139226+ShotikoLabadze@users.noreply.github.com>
2022-04-20 13:25:10 +0400

    Randomize holes

---
+++ script.js
@@ -0,0 +1,11 @@
+var block = document.getElementById("block");
+var hole = document.getElementById("hole");
+var character = document.getElementById("character");
+var jumping = 0;
+var counter = 0;
+
+hole.addEventListener("animationiteration", () => {
+  var random = -(Math.random() * 300 + 150);
+  hole.style.top = random + "px";
+  counter++;
+});
